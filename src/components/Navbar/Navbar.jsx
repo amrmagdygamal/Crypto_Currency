5c1dc539134a351feec5d7e133b8cc0bb29f2a2c
amrMagdy <amroraker@gmail.com>
2023-03-22 21:44:15 +0200

    Adding the navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import { Avatar, Menu, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+import icon from '../../assets/cryptocurrency.png.url'
+import { HomeOutlined } from '@ant-design/icons';
+import { FundOutlined, MoneyCollectOutlined, BulbOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div className='nav__container'>
+      <div className="nav__logo">
+
+        <Avatar src={icon} />
+
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Crytoverse</Link>
+        </Typography.Title>
+
+      </div>
+
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+        
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
+
---
+++ src/container/index.js
@@ -0,0 +1,17 @@
+
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  SpecialMenu
+};
